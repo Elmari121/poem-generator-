e12b42a3a09817027b242fa8f6456a6032df4f20
Elmari121 <robbertselmari@gmail.com>
2025-03-26 14:47:23 +0200

    add typewriter

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 function generatePoem(event) {
   event.preventDefault();
+
   let poemElement = document.querySelector("#poem");
-  poemElement.innerHTML = "roses are red";
+  poemElement.innerHTML = "";
+
+  const typewriter = new Typewriter(poemElement, {
+    loop: false,
+    delay: 75,
+  });
+
+  typewriter
+    .typeString("roses are red")
+    .pauseFor(500)
+    .typeString("<br>violets are blue")
+    .pauseFor(500)
+    .typeString("<br>and AI poems are fun!")
+    .start();
 }
+
 let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
