9527646e247f75ce3f84d02d6de2a6f13c33f336
Elmari121 <robbertselmari@gmail.com>
2025-03-27 11:05:17 +0200

    import axios apikey and apiurl

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
+function displaypoem(response) {
+  console.log("poem generated");
+  typewriter
+    .typeString(response.data.answer)
+    .pauseFor(500)
+
+    .pauseFor(500)
+
+    .start();
+}
+const typewriter = new typewriter(poemElement, {
+  loop: false,
+  delay: 75,
+});
 function generatePoem(event) {
   event.preventDefault();
+  let instructionsinput = document.querySelector("user-instructions");
+  let apikey = "30927dtfa44b4770359oe8258a9c5b2c";
+  let prompt = "generate a poem about $(instructionsinput.value)";
+  let context = "";
+  let apiurl =
+    "https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}";
+  console.log("generating poem");
 
+  axios.get(apiurl).then(displaypoem);
   let poemElement = document.querySelector("#poem");
   poemElement.innerHTML = "";
 
-  const typewriter = new Typewriter(poemElement, {
-    loop: false,
-    delay: 75,
-  });
-
-  typewriter
-    .typeString("roses are red")
-    .pauseFor(500)
-    .typeString("<br>violets are blue")
-    .pauseFor(500)
-    .typeString("<br>and AI poems are fun!")
-    .start();
+  let poemFormElement = document.querySelector("#poem-generator-form");
+  poemFormElement.addEventListener("submit", generatePoem);
 }
-
-let poemFormElement = document.querySelector("#poem-generator-form");
-poemFormElement.addEventListener("submit", generatePoem);
